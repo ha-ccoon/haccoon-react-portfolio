c0015e046fb7f82ad89b5d8310a03107ef9e5b15
lien-mhsohn <haccoon@lien.kr>
2024-07-26 17:53:25 +0900

    chore: add a font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import type { Metadata } from 'next';
+import { IBM_Plex_Sans_KR } from 'next/font/google';
+
+const inter = IBM_Plex_Sans_KR({
+  weight: ['400', '500', '600'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Kujin Ko',
@@ -11,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         {/* Layout UI */}
         <main>{children}</main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <div>
+      <h1>Kujin Ko</h1>
       <h1>고규진</h1>
     </div>
   );
