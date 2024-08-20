f01540d1b2860396ecbc91981048a7de288a8230
ha-ccoon <codeminhasohn@gmail.com>
2024-08-20 21:51:20 +0900

    chore: set a default font

--- engineer/app/layout.tsx
+++ engineer/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 
 import NavigationBar from '@/components/navigation/NavigationBar';
 
 import '@/app/global.scss';
 import './app.scss';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
 
 export const metadata: Metadata = {
   title: 'Minha Sohn',
