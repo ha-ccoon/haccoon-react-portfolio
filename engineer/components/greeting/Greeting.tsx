6dc1f69bd08ac43de0d1430c205959fd84aa1c47
ha-ccoon <codeminhasohn@gmail.com>
2024-08-27 12:06:12 +0900

    style: modify the greeting component CSS

--- engineer/components/greeting/Greeting.tsx
+++ engineer/components/greeting/Greeting.tsx
@@ -2,31 +2,42 @@ import React from 'react';
 
 import Image from 'next/image';
 
+import { Text, Title } from '@mantine/core';
+
 import './greeting.scss';
-import { Button } from '@mantine/core';
 
 export function Greeting() {
   return (
-    <div className="greeting-container">
-      <div>
-        <Image
-          className="greeting-image"
-          src="/haccoon.jpg"
-          alt={'haccoon image'}
-          height={120}
-          width={120}
-          priority={true}
-        />
-      </div>
+    <React.Fragment>
+      <Title className="greeting-title" order={1}>
+        Hi, I am &nbsp;<span className="name">Minha Sohn</span>
+      </Title>
 
-      <section className="greeting-section">
-        <h1 className="greeting-title">Hi, I am Minha Sohn</h1>
-        <div className="greeting-subsection">
-          <h2 className="greeting-subtitle">Full-Stack Engineer</h2>
-          <p className="greeting-description">Specialized in Healthcare</p>
+      <div className="greeting-container">
+        <div className="greeting-image-div">
+          <Image
+            className="greeting-image"
+            src="/haccoon.jpg"
+            alt={'haccoon image'}
+            height={120}
+            width={120}
+            priority={true}
+          />
         </div>
-        <Button>안녕하세요 눌러주세요</Button>
-      </section>
-    </div>
+
+        <section className="greeting-section">
+          <div className="greeting-sub-div">
+            <Title order={2}>Full-Stack Engineer</Title>
+            <Title order={3}>Specialized in Healthcare</Title>
+            <Text className="greeting-description-text">
+              Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta
+              facilis perspiciatis corporis nulla aspernatur. Autem eligendi
+              rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque
+              perspiciatis! Maiores minima consectetur.
+            </Text>
+          </div>
+        </section>
+      </div>
+    </React.Fragment>
   );
 }
