01a11c1c221d836f3fc2e043088953f172213de7
ha-ccoon <codeminhasohn@gmail.com>
2024-09-14 09:57:54 +0900

    chore: add contact files

---
+++ engineer/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ContactComponent from '@/components/contact/ContactComponent';
+
+export default function Contact() {
+  return (
+    <React.Fragment>
+      <ContactComponent />
+    </React.Fragment>
+  );
+}
---
+++ engineer/components/contact/ContactComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ContactComponent() {
+  return (
+    <React.Fragment>
+      <p>contact</p>
+    </React.Fragment>
+  );
+}
