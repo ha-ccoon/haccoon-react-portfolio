15cc98030b054805d565db5763c8e3ec055264d0
ha-ccoon <codeminhasohn@gmail.com>
2024-09-20 19:53:40 +0900

    chore: refactor actionIcons codes

--- engineer/components/contact/ContactComponent.tsx
+++ engineer/components/contact/ContactComponent.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-import { ActionIcon, Container, Space, Text, Title } from '@mantine/core';
+import Image from 'next/image';
+
+import { Container, Space, Text, Title } from '@mantine/core';
+
+import { LinkIcons } from './LinkIcons';
 
 import '@/components/contact/contact.scss';
-import {
-  IconBrandGithub,
-  IconBrandLinkedin,
-  IconMailHeart,
-} from '@tabler/icons-react';
-import Image from 'next/image';
 
 export default function ContactComponent() {
   return (
@@ -45,42 +43,7 @@ export default function ContactComponent() {
       <Text>Full Stack Engineer specialized in Healthcare</Text>
       {/* <Text>True Korean, Honorary Canadian</Text> */}
 
-      <Container
-        classNames={{
-          root: 'container',
-        }}
-      >
-        <ActionIcon
-          color={`var(--mantine-color-softPink-6)`}
-          component="a"
-          href={process.env.LINKEDIN}
-          target="blank"
-          size={50}
-          variant="filled"
-        >
-          <IconBrandLinkedin size={40} />
-        </ActionIcon>
-        <ActionIcon
-          color={`var(--mantine-color-softPink-6)`}
-          component="a"
-          href={process.env.GITHUB}
-          target="blank"
-          size={50}
-          variant="filled"
-        >
-          <IconBrandGithub size={40} />
-        </ActionIcon>
-        <ActionIcon
-          color={`var(--mantine-color-softPink-6)`}
-          component="a"
-          href={process.env.EMAIL}
-          target="blank"
-          size={50}
-          variant="filled"
-        >
-          <IconMailHeart size={40} />
-        </ActionIcon>
-      </Container>
+      <LinkIcons />
 
       <Text>&copy; 2024 by haccoon. All Rights Reserved.</Text>
 
