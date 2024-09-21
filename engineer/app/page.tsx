ef6475b5fb32121441c58a12fe79021aee31ba7c
ha-ccoon <codeminhasohn@gmail.com>
2024-09-21 11:52:21 +0900

    chore: implement AppShell
    - implement a navigation bar as a header

--- engineer/app/page.tsx
+++ engineer/app/page.tsx
@@ -1,21 +1,82 @@
+'use client';
+
 import React from 'react';
 
+import Image from 'next/image';
+
+import {
+  ActionIcon,
+  AppShell,
+  AppShellHeader,
+  AppShellMain,
+  Burger,
+  Group,
+  rem,
+  UnstyledButton,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+import Greeting from '@/components/greeting/Greeting';
 import Education from '@/app/education/page';
 import Experience from '@/app/experience/page';
-import Greeting from '@/components/greeting/Greeting';
 import Skills from '@/app/skills/page';
-import NavigationBar from '@/components/navigation/NavigationBar';
 import Contact from '@/app/contact/page';
 
+import '@/app/app.scss';
+
 export default function Main() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <React.Fragment>
-      <NavigationBar />
-      <Greeting />
-      <Skills />
-      <Experience />
-      <Education />
-      <Contact />
-    </React.Fragment>
+    <AppShell
+      header={{ height: rem(90) }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { desktop: true, mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShellHeader>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Group
+            classNames={{
+              root: 'header-group',
+            }}
+            justify="space-between"
+            style={{ flex: 1 }}
+          >
+            <ActionIcon
+              component="a"
+              href={'/'}
+              size={60}
+              variant="transparent"
+            >
+              <Image
+                src="/haccoon.png"
+                alt={'logo'}
+                width={60}
+                height={60}
+              ></Image>
+            </ActionIcon>
+            <Group ml="xl" gap={rem(30)}>
+              <UnstyledButton>Skills</UnstyledButton>
+              <UnstyledButton>Experience</UnstyledButton>
+              <UnstyledButton>Education</UnstyledButton>
+              <UnstyledButton>Contact</UnstyledButton>
+            </Group>
+          </Group>
+        </Group>
+      </AppShellHeader>
+      <AppShellMain>
+        {/* <NavigationBar /> */}
+        <Greeting />
+        <Skills />
+        <Experience />
+        <Education />
+        <Contact />
+      </AppShellMain>
+    </AppShell>
   );
 }
