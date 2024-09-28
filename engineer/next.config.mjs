ff75f4d8eeb6ff79fa5e1f389ba2d6fcf6196fcd
ha-ccoon <codeminhasohn@gmail.com>
2024-09-28 14:14:49 +0900

    chore: implement firestore admin
    - add admin key
    - add wepack configuration in next.config.mjs

--- engineer/next.config.mjs
+++ engineer/next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   sassOptions: {
     prependData: `@import "./_mantine.scss";`,
   },
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
