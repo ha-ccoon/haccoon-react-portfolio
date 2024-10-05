c78640050b1516503c48d7c07553f24de0d8b092
ha-ccoon <codeminhasohn@gmail.com>
2024-10-05 19:57:11 +0900

    chore: implement the regular firebase connection
    - annotate admin code

--- engineer/firebase/admin.ts
+++ engineer/firebase/admin.ts
@@ -1,18 +1,18 @@
-import admin, { ServiceAccount } from 'firebase-admin';
+// import admin, { ServiceAccount } from 'firebase-admin';
 
-import serviceAccount from './engineer-portfolio-admin-key.json';
-// var serviceAccount = require(`${process.env.NEXT_PUBLIC_ADMINKEY}`);
+// import serviceAccount from './engineer-portfolio-admin-key.json';
+// // var serviceAccount = require(`${process.env.NEXT_PUBLIC_ADMINKEY}`);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
-  databaseURL: 'https://items.firebaseio.com',
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount as ServiceAccount),
+//   databaseURL: 'https://items.firebaseio.com',
+// });
 
-// As an admin, the app has access to read and write all data, regardless of Security Rules
-const db = admin.database();
-const ref = db.ref('restricted_access/secret_document');
-ref.once('value', function (snapshot) {
-  console.log(snapshot.val());
-});
+// // As an admin, the app has access to read and write all data, regardless of Security Rules
+// const adminDb = admin.database();
+// const ref = adminDb.ref('restricted_access/secret_document');
+// ref.once('value', function (snapshot) {
+//   console.log(snapshot.val());
+// });
 
-export default db;
+// export default adminDb;
